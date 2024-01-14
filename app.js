cbf954a3c4416feb3906481c16b37ecd3e8f1b8e
Atif-1 <atif1712001@gmail.com>
2024-01-14 20:33:02 +0530

    Filtering Paths using express js

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const http =require('http');
 
 //express modules
 const express=require('express');
-const bodyParser= require('body-parser');
 const app=express();
+
+const bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-	res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type=submit>Add Product</button></form></body></html>');
-})
-app.use('/product',(req,res,next)=>{
-	console.log(req.body);
-	res.redirect('/add-product');
-})
+const adminRoutes=require('./routes/admin.js');
+const shopRoutes=require('./routes/shop.js');
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.use('/',(req,res,next)=>{
-	res.send('<h1>Hello from Express Js</h1>');
+app.use((req,res,next)=>{
+	res.status(404).send('<h1>Page Not Found</h1>');
 })
-
 app.listen(3000);
 
