79135d3677ada940d47232b363691f6ca8d7ebaf
Atif-1 <atif1712001@gmail.com>
2024-01-14 13:52:37 +0530

    Express js Practise

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const http =require('http');
 
 //express modules
 const express=require('express');
-
+//middleware 1
 const app=express();
 app.use((req,res,next)=>{
 	console.log("In the middleware");
 	next();
 })
+//middleware 2
 app.use((req,res,next)=>{
 	console.log("In the another middleware");
 	res.send('<h1>Hello From Express!</h1>');
