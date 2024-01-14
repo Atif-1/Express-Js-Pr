4ff95c4fa93fc3ecb0b5c2c46535a45ffde26bba
Atif-1 <atif1712001@gmail.com>
2024-01-14 19:38:47 +0530

    Routing request using express js

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const http =require('http');
 
 //express modules
 const express=require('express');
-//middleware 1
+const bodyParser= require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-	console.log("In the middleware");
-	next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+	res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type=submit>Add Product</button></form></body></html>');
 })
-//middleware 2
-app.use((req,res,next)=>{
-	console.log("In the another middleware");
-	res.send('<h1>Hello From Express!</h1>');
+app.use('/product',(req,res,next)=>{
+	console.log(req.body);
+	res.redirect('/add-product');
 })
+
+app.use('/',(req,res,next)=>{
+	res.send('<h1>Hello from Express Js</h1>');
+})
+
 app.listen(3000);
 
